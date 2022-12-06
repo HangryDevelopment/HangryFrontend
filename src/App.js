59d1e6c78e17f72b17888440a97647ce1e127fb5
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-12-05 23:45:20 -0600

    added link to code

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ function App() {
                     <a className="navbar-brand" href="https://github.com/Chase-Forestello" target="_blank">
                         <span id="navbarLogo">&lt; / &gt;</span><span className="navbar-text">Chase Forestello</span>
                     </a>
+                    <a href="https://github.com/HangryDevelopment/HangryFrontend" target="_blank">
+                        <span className={"navbar-text"} id={"navbarCode"}>[CODE]</span>
+                    </a>
                 </div>
             </nav>
             <div className="card outerCard">
