f0858a6f62252796f32752765746dd17638e7969
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-12-05 23:06:43 -0600

    built out frontend, fetching from yelp using axios. Added gh-pages deploy script

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import AxiosTest from "./components/AxiosTest";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <AxiosTest/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <nav className="navbar sticky-top bg-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="https://github.com/Chase-Forestello" target="_blank">
+                        <span id="navbarLogo">&lt; / &gt;</span><span className="navbar-text">Chase Forestello</span>
+                    </a>
+                </div>
+            </nav>
+            <div className="card outerCard">
+                <div className="card innerCard">
+                    <AxiosTest/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/components/AxiosTest.js
+++ src/components/AxiosTest.js
@@ -1,32 +1,82 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 
-export default function AxiosTest(props) {
-    // ec2 dns
-    // const url = 'http://ec2-54-90-98-222.compute-1.amazonaws.com:8080/api/users/';
-    //
-    // const getAllUsers = () => {
-    //     axios.get(url).then((response) => {
-    //          const allUsers = response.data;
-    //         console.log(allUsers);
-    //     }).catch(error => console.error(`Error: ${error}`));
-    // }
-    // getAllUsers();
+export default function RestaurantSearch(props) {
+    const [searchLoc, setSearchLoc] = useState("");
+    const [url, setUrl] = useState(``)
+    const [selection, setSelection] = useState('Hangry?');
+    const handleChange = (event) => {
+        setSearchLoc(event.target.value);
+    }
+    useEffect(() => {
+        async function getYelpData() {
+            await axios.get(`${url}`, {
+                headers: {
+                    Authorization: `Bearer ${process.env.REACT_APP_YELP_FUSION_API_KEY}`
 
-        //UI feedback to tell the user when we are retrieving infromation from the API
-
-        //using a proxy server cors-anywhere to get rid of the CROS probblem
-        //SUPER HOT TIP: passing the location variable, which equals to the user's input (see below). Instead of grabbbing the entire API, it will only retrieve the restaurants that are closed to the lcoation information we entered. This makes the lodading wayyyyyyy faster.
-    const options = {
-        method: 'GET',
-        headers: {
-            accept: 'application/json',
-            Authorization: `Bearer ${process.env.REACT_APP_YELP_FUSION_API_KEY}`
+                },
+            }).then((res) => {
+                const random = Math.floor(Math.random() * res.data.businesses.length);
+                console.log("Hanrgy selection: " + res.data.businesses[random].name)
+                setSelection(res.data.businesses[random].name)
+            }).catch((err) => {
+                console.log('error')
+            })
         }
+
+        getYelpData();
+    }, [url])
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(searchLoc)
+        setUrl(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=${searchLoc}&term=fast%20food&radius=40000&categories=Fast%20food&open_now=true&sort_by=best_match&matches_party_size_param=true&limit=20`)
+
+        // const options = {
+        //     method: 'GET',
+        //     headers: {
+        //         accept: 'application/json',
+        //         Authorization: `Bearer ${process.env.REACT_APP_YELP_FUSION_API_KEY}`
+        //     }
+        // };
+        //
+        // fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=${searchLoc}&term=fast%20food&radius=40000&categories=Fast%20food&open_now=true&sort_by=best_match&matches_party_size_param=true&limit=20`, options)
+        //     .then(response => response.json())
+        //     .then(response => console.log(response))
+        //     .then(response => places = response.businesses)
+        //     .catch(err => console.error(err));
+        // console.log(places);
     };
 
-    fetch('https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=San%20Antonio%2C%20TX%2078218&term=fast%20food&radius=25000&categories=Fast%20food&open_now=true&sort_by=best_match&matches_party_size_param=true&limit=20', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+    return (
+        <div className={"text-center"}>
+            <form id={"searchForm"} onSubmit={handleSubmit}>
+                <input
+                    id="searchInput"
+                    type="text"
+                    name="Search Location"
+                    placeholder="Search Location"
+                    value={searchLoc}
+                    onChange={handleChange}
+                />
+                <button className={"submitBtn"} type={"submit"}>Hangry!</button>
+            </form>
+            <div className={"card selectionCard"}>{selection}</div>
+        </div>
+    )
+
 }
+// ec2 dns
+// const url = 'http://ec2-54-90-98-222.compute-1.amazonaws.com:8080/api/users/';
+//
+// const getAllUsers = () => {
+//     axios.get(url).then((response) => {
+//          const allUsers = response.data;
+//         console.log(allUsers);
+//     }).catch(error => console.error(`Error: ${error}`));
+// }
+// getAllUsers();
+
+//UI feedback to tell the user when we are retrieving infromation from the API
+
+//using a proxy server cors-anywhere to get rid of the CROS probblem
+//SUPER HOT TIP: passing the location variable, which equals to the user's input (see below). Instead of grabbbing the entire API, it will only retrieve the restaurants that are closed to the lcoation information we entered. This makes the lodading wayyyyyyy faster.
