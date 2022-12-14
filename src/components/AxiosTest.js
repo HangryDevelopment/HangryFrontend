203e6346140bcb8aa2c4710f70e217e5fc466a53
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-12-14 16:27:31 -0600

    Frontend using ubuntu ec2 for yelp fetch

--- src/components/AxiosTest.js
+++ src/components/AxiosTest.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
+import {HANGRY_LOCAL_API, HANGRY_UBUNTUEC2_API} from "../public_constants";
 
 export default function RestaurantSearch(props) {
     let userLat;
@@ -38,7 +39,8 @@ export default function RestaurantSearch(props) {
                 console.log('error')
             })
         }
-        async function getYelpRandom(){
+
+        async function getYelpRandom() {
             await axios.get(`${url}`).then((res) => {
                 const random = Math.floor(Math.random() * res.data.businesses.length);
                 console.log("Hanrgy selection: " + res.data.businesses[random].name)
@@ -47,6 +49,7 @@ export default function RestaurantSearch(props) {
                 console.log('error')
             })
         }
+
         getYelpRandom();
         // getYelpData();
     }, [url])
@@ -55,22 +58,7 @@ export default function RestaurantSearch(props) {
     }, [])
     const handleSubmit = (event) => {
         event.preventDefault();
-        setUrl(`http://ec2-3-135-20-154.us-east-2.compute.amazonaws.com:8080/api/yelpFetch/${searchLoc}`)
-
-        // const options = {
-        //     method: 'GET',
-        //     headers: {
-        //         accept: 'application/json',
-        //         Authorization: `Bearer ${process.env.REACT_APP_YELP_FUSION_API_KEY}`
-        //     }
-        // };
-        //
-        // fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=${searchLoc}&term=fast%20food&radius=40000&categories=Fast%20food&open_now=true&sort_by=best_match&matches_party_size_param=true&limit=20`, options)
-        //     .then(response => response.json())
-        //     .then(response => console.log(response))
-        //     .then(response => places = response.businesses)
-        //     .catch(err => console.error(err));
-        // console.log(places);
+        setUrl(HANGRY_UBUNTUEC2_API + searchLoc)
     };
     return (
 
@@ -91,18 +79,3 @@ export default function RestaurantSearch(props) {
     )
 
 }
-// ec2 dns
-// const url = 'http://ec2-54-90-98-222.compute-1.amazonaws.com:8080/api/users/';
-//
-// const getAllUsers = () => {
-//     axios.get(url).then((response) => {
-//          const allUsers = response.data;
-//         console.log(allUsers);
-//     }).catch(error => console.error(`Error: ${error}`));
-// }
-// getAllUsers();
-
-//UI feedback to tell the user when we are retrieving infromation from the API
-
-//using a proxy server cors-anywhere to get rid of the CROS probblem
-//SUPER HOT TIP: passing the location variable, which equals to the user's input (see below). Instead of grabbbing the entire API, it will only retrieve the restaurants that are closed to the lcoation information we entered. This makes the lodading wayyyyyyy faster.
