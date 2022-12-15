0df26fcd33bc8e54712099ab8008a877b91bc380
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-12-15 17:40:52 -0600

    fixing mixed content

--- src/public_constants.js
+++ src/public_constants.js
@@ -1,2 +1,2 @@
-export const HANGRY_UBUNTUEC2_API = `//ec2-3-135-20-154.us-east-2.compute.amazonaws.com:8080/api/yelpFetch/`;
-export const HANGRY_LOCAL_API = `//localhost:8080/api/yelpFetch/`;
+export const HANGRY_UBUNTUEC2_API = `http://ec2-3-135-20-154.us-east-2.compute.amazonaws.com:8080/api/yelpFetch/`;
+export const HANGRY_LOCAL_API = `http://localhost:8080/api/yelpFetch/`;
