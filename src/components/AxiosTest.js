f1a457418ab92cc9e19ffeaa74a25d6a2be39a60
Chase Forestello <chase.forestello@gmail.com>
2023-03-27 17:14:08 -0500

    modified endpoint url

--- src/components/AxiosTest.js
+++ src/components/AxiosTest.js
@@ -1,85 +1,96 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import {HANGRY_LOCAL_API, HANGRY_UBUNTUEC2_API, HANGRY_UBUNTUEC2_CLOUDFLARE_API} from "../public_constants";
+import { HANGRY_LOCAL_API, HANGRY_LINUXEC2_API } from "../public_constants";
 
 export default function RestaurantSearch(props) {
-    let userLat;
-    let userLong;
+  let userLat;
+  let userLong;
 
-    const [url, setUrl] = useState("")
-    const [searchLoc, setSearchLoc] = useState("");
-    const [selection, setSelection] = useState('Hangry?');
-    const [yelpUrl, setYelpUrl] = useState("https://www.yelp.com/");
+  const [url, setUrl] = useState("");
+  const [searchLoc, setSearchLoc] = useState("");
+  const [selection, setSelection] = useState("Hangry?");
+  const [yelpUrl, setYelpUrl] = useState("https://www.yelp.com/");
 
-    const successCallback = (position) => {
-        userLat = position.coords.latitude;
-        userLong = position.coords.longitude;
-        console.log(`${userLat}, ${userLong}`)
-        setSearchLoc(`${userLat}, ${userLong}`)
-    };
-    const errorCallback = (error) => {
-        console.log(error);
-    };
+  const successCallback = (position) => {
+    userLat = position.coords.latitude;
+    userLong = position.coords.longitude;
+    console.log(`${userLat}, ${userLong}`);
+    setSearchLoc(`${userLat}, ${userLong}`);
+  };
+  const errorCallback = (error) => {
+    console.log(error);
+  };
 
+  const handleChange = (event) => {
+    setSearchLoc(event.target.value);
+  };
+  useEffect(() => {
+    // async function getYelpData() {
+    //     await axios.get(`${url}`, {
+    //         headers: {
+    //             Authorization: `Bearer ${process.env.REACT_APP_YELP_FUSION_API_KEY}`
+    //
+    //         },
+    //     }).then((res) => {
+    //         const random = Math.floor(Math.random() * res.data.businesses.length);
+    //         console.log("Hanrgy selection: " + res.data.businesses[random].name)
+    //         setSelection(res.data.businesses[random].name)
+    //     }).catch((err) => {
+    //         console.log('error')
+    //     })
+    // }
 
-    const handleChange = (event) => {
-        setSearchLoc(event.target.value);
+    async function getYelpRandom() {
+      await axios
+        .get(`${url}`)
+        .then((res) => {
+          const random = Math.floor(Math.random() * res.data.businesses.length);
+          console.log(
+            "Hanrgy selection: " +
+              res.data.businesses[random].name +
+              ` [${random}]`
+          );
+          for (let i = 0; i < res.data.businesses.length; i++) {
+            console.log(res.data.businesses[i].name);
+          }
+          setSelection(res.data.businesses[random].name);
+          setYelpUrl(res.data.businesses[random].url);
+        })
+        .catch((err) => {
+          console.log("error");
+        });
     }
-    useEffect(() => {
-        // async function getYelpData() {
-        //     await axios.get(`${url}`, {
-        //         headers: {
-        //             Authorization: `Bearer ${process.env.REACT_APP_YELP_FUSION_API_KEY}`
-        //
-        //         },
-        //     }).then((res) => {
-        //         const random = Math.floor(Math.random() * res.data.businesses.length);
-        //         console.log("Hanrgy selection: " + res.data.businesses[random].name)
-        //         setSelection(res.data.businesses[random].name)
-        //     }).catch((err) => {
-        //         console.log('error')
-        //     })
-        // }
 
-        async function getYelpRandom() {
-            await axios.get(`${url}`).then((res) => {
-                const random = Math.floor(Math.random() * res.data.businesses.length);
-                console.log("Hanrgy selection: " + res.data.businesses[random].name + ` [${random}]`)
-                for (let i = 0; i < res.data.businesses.length; i++) {
-                    console.log(res.data.businesses[i].name)
-                }
-                setSelection(res.data.businesses[random].name)
-                setYelpUrl(res.data.businesses[random].url)
-            }).catch((err) => {
-                console.log('error')
-            })
-        }
-
-        getYelpRandom();
-        // getYelpData();
-    }, [url])
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-    }, [])
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        setUrl(HANGRY_UBUNTUEC2_CLOUDFLARE_API + searchLoc)
-    };
-    return (
-        <div className={"text-center"}>
-            <form id={"searchForm"} onSubmit={handleSubmit}>
-                <input
-                    id="searchInput"
-                    type="text"
-                    name="Search Location"
-                    placeholder="Search Location"
-                    value={searchLoc}
-                    onChange={handleChange}
-                />
-                <button className={"submitBtn"} type={"submit"}>Hangry!</button>
-            </form>
-            <div className={"card selectionCard"}><a href={yelpUrl} target={"_blank"}>{selection}</a></div>
-        </div>
-    )
-
+    getYelpRandom();
+    // getYelpData();
+  }, [url]);
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+  }, []);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setUrl(HANGRY_LINUXEC2_API + searchLoc);
+  };
+  return (
+    <div className={"text-center"}>
+      <form id={"searchForm"} onSubmit={handleSubmit}>
+        <input
+          id="searchInput"
+          type="text"
+          name="Search Location"
+          placeholder="Search Location"
+          value={searchLoc}
+          onChange={handleChange}
+        />
+        <button className={"submitBtn"} type={"submit"}>
+          Hangry!
+        </button>
+      </form>
+      <div className={"card selectionCard"}>
+        <a href={yelpUrl} target={"_blank"}>
+          {selection}
+        </a>
+      </div>
+    </div>
+  );
 }
--- src/public_constants.js
+++ src/public_constants.js
@@ -1,2 +1,2 @@
-export const HANGRY_UBUNTUEC2_CLOUDFLARE_API = `http://ec2-3-135-20-154.us-east-2.compute.amazonaws.com:8080/api/yelpFetch/`;
+export const HANGRY_LINUXEC2_API = `https://ec2-3-145-103-141.us-east-2.compute.amazonaws.com:8080/api/yelpFetch/`;
 export const HANGRY_LOCAL_API = `http://localhost:8080/api/yelpFetch/`;
