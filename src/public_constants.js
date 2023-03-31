18aa23ba08bc97550c00c34e6f34455ef7918fc7
Chase Forestello <chase.forestello@gmail.com>
2023-03-31 13:31:51 -0500

    formatted

--- src/public_constants.js
+++ src/public_constants.js
@@ -1,2 +1,2 @@
 export const HANGRY_LOCAL_API = `http://localhost:8080/api/yelpFetch/`;
-export const HANGRY_GLITCH_API = `https://open-cyclic-barberry.glitch.me/api/yelpFetch/`
+export const HANGRY_GLITCH_API = `https://open-cyclic-barberry.glitch.me/api/yelpFetch/`;
