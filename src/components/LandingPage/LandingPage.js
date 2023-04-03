4d60136136f6514d03343b4a3bc03bbf110da0e2
Chase Forestello <chase.forestello@gmail.com>
2023-04-03 18:14:46 -0500

    adjusted scss for each component + adding landing page

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import "./LandingPage.scss";
+
+const LandingPage = () => {
+  return (
+    <div className="container">
+      <div className="card">Hello</div>
+    </div>
+  );
+};
+
+export default LandingPage;
