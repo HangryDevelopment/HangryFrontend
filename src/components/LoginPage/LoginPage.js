f083398c3e293ce969eea4fd909e5ccb9b56e18b
Chase Forestello <chase.forestello@gmail.com>
2023-04-06 17:52:57 -0500

    working on login

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,41 +1,48 @@
 import React from "react";
 import "./LoginPage.scss";
-import { USER_API_BASE_URL_LOCAL, USER_API_BASE_URL_GLITCH } from "../../public_constants";
+import {
+  USER_API_BASE_URL_LOCAL,
+  USER_API_BASE_URL_GLITCH,
+} from "../../public_constants";
 
 const LoginPage = () => {
+  const handleLogin = (event) => {
+    event.preventDefault();
+  };
   const handleSubmit = (event) => {
     event.preventDefault();
     let username = document.querySelector(".username-input").value;
-    let email = document.querySelector(".email-input").value;
-    let region = document.querySelector(".region-input").value;
+    let password = document.querySelector(".password-input").value;
+    if (username.length === 0 || password.length === 0) {
+      alert("Username and password cannot be blank!");
+      return;
+    } else {
+      console.log(username, password);
 
-    console.log(username, email, region);
-
-    let newUser = {
-      userName: username,
-      email: email,
-      region: region,
-      blocked: false,
-    };
-    let request = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newUser),
-    };
-    fetch(USER_API_BASE_URL_GLITCH + "create", request).then((response) => {
-      if (response.status !== 200) {
-        alert("Registration failed try again: " + response.status);
-        console.log(response.statusText);
-      } else {
-        console.log(response.status);
-        alert("Success!");
-      }
-    });
+      let newUser = {
+        userName: username,
+        password: password,
+      };
+      let request = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newUser),
+      };
+      fetch(USER_API_BASE_URL_GLITCH + "create", request).then((response) => {
+        if (response.status !== 200) {
+          alert("Registration failed try again: " + response.status);
+          console.log(response.statusText);
+        } else {
+          console.log(response.status);
+          alert("Success!");
+        }
+      });
+    }
   };
   return (
     <div className="card container login-page-cont">
       <div className="login-test-cont card">
-        <form onSubmit={handleSubmit}>
+        <form>
           <label htmlFor="username">Username:</label>
           <input
             className="username-input"
@@ -43,22 +50,26 @@ const LoginPage = () => {
             name="username"
             placeholder="Username"
           />
-          <label htmlFor="email">Email:</label>
+          <label htmlFor="password">Password:</label>
           <input
-            className="email-input"
-            type="text"
-            name="email"
-            placeholder="Email"
+            className="password-input"
+            type="password"
+            name="password"
+            placeholder="Password"
           />
-          <label htmlFor="region">Region:</label>
-          <input
-            className="region-input"
-            type="text"
-            name="region"
-            placeholder="Region"
-          />
-          <button className="user-submit-btn" type="submit">
-            Submit
+          <button
+            className="user-submit-btn"
+            type="submit"
+            onClick={handleSubmit}
+          >
+            Register
+          </button>
+          <button
+            className="user-submit-btn"
+            type="submit"
+            onClick={handleLogin}
+          >
+            Login
           </button>
         </form>
       </div>
