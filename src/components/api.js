7d1feca92b7410438e4424b63fce7935f349f819
Chase Forestello <chase.forestello@gmail.com>
2023-04-12 12:08:18 -0500

    commented out api.js

--- src/components/api.js
+++ src/components/api.js
@@ -1,7 +1,7 @@
-import axios from "axios";
+// import axios from "axios";
 
-const api = axios.create({
-  baseURL: "/api",
-});
+// const api = axios.create({
+//   baseURL: "/api",
+// });
 
-export default api;
+// export default api;
